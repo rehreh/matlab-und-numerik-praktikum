e76ee8b67357e6e2bf7cf2e9da67eec8d5a957e4
Croff96 <peter.26180@web.de>
2020-06-24 08:35:57 +0200

    Update Oszillator.m

--- Oszillator.m
+++ Oszillator.m
@@ -4,11 +4,13 @@ d = 0.3;
 k = 2;
 m = 1;
 
+options = odeset('AbsTol', 1e-6, 'RelTol', 1e-6);
+
 odefun_O = @(t,z) odefun_Oszillator (t, z, d, k, m);
 
 tspan = [0,30];     % Zeitintervall für die Integration
 z0 = [1;-3];         % Anfangswerte
-[t,z] = ode45 (odefun_O, tspan, z0);
+[t,z] = ode45 (odefun_O, tspan, z0, options);
 
 plot (t,z(:,1), 'g');
 
